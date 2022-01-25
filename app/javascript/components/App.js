4faa24996212ee582b757d9290322dda9384c63f
JuliCarracedo <julicarra@hotmail.com>
2022-01-25 15:14:11 -0300

    connect store to app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,16 +2,22 @@ import React from "react"
 import Greeting from "./Greeting";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from 'react-redux'
+import store from "../redux/configureStore";
+
+
 
 const App = () => {
 
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path='/' element={<>Welcome to my app</>} />
-          <Route path='/greet' element={<Greeting message='App has been connected' />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store} >
+        <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<>Welcome to my app</>} />
+            <Route path='/greet' element={<Greeting message='App has been connected' />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
 
 }

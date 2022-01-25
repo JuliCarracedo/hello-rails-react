c06e1f8e527be893a1bca1b8c21bd8ab934a9897
JuliCarracedo <julicarra@hotmail.com>
2022-01-25 14:42:51 -0300

    install and add react router dom

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Greeting from "./Greeting";
 
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
 
     return (
-      <React.Fragment>
-        <Greeting message='App has been connected' />
-      </React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<>Welcome to my app</>} />
+          <Route path='/greet' element={<Greeting message='App has been connected' />} />
+        </Routes>
+      </BrowserRouter>
     );
 
 }

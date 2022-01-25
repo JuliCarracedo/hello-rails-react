66d6df7686f6219698a3b40489bb73dadc38c7b7
JuliCarracedo <julicarra@hotmail.com>
2022-01-25 14:29:08 -0300

    add app as func component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Greeting from "./Greeting";
+
+
+
+const App = () => {
+
+    return (
+      <React.Fragment>
+        <Greeting message='App has been connected' />
+      </React.Fragment>
+    );
+
+}
+
+export default App
